d0bd5dc2262189023a590021e080ede73bde2987
嘟嘟噜 <lensevenlau@gmail.com>
2018-06-17 21:41:05 -0400

    change

--- chair_price_web/src/models/users/user.py
+++ chair_price_web/src/models/users/user.py
@@ -5,7 +5,7 @@ class User:
     def __init__(self, email, password, _id):
         self.email = email
         self.password = password
-        self._id = uuid.uuid4().hex
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<User {}>".format(self.email)
